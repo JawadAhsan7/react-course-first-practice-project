35ad1abfb60c54ac71d8ae36876b2fa57a601607
Jawad Ahsan <jahsandev@gmail.com>
2023-07-06 11:52:00 +0500

    move table into it's separate new component Table

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Form from "./Components/Form";
 import Header from "./Components/Header";
+import Table from "./Components/Table";
 
 function App() {
   const calculateHandler = (userInput) => {
@@ -36,26 +37,7 @@ function App() {
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+      <Table />
     </div>
   );
 }
